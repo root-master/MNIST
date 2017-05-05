89b441b463f0f635d988dd90ff32a0419dbc3228
Jacob Rafati <yrafati@gmail.com>
2017-05-05 11:44:23 -0700

    train all data

--- mnist_cnn_Keras.py
+++ mnist_cnn_Keras.py
@@ -95,14 +95,21 @@ model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 
+"""
 model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           verbose=1,
           validation_data=(x_val, y_val))
-score = model.evaluate(x_val, y_val, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+"""
+#score = model.evaluate(x_val, y_val, verbose=0)
+#print('Test loss:', score[0])
+#print('Test accuracy:', score[1])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1)
 
 
 predictions = model.predict_classes(x_test, verbose=0)
